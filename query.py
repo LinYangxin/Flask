f82b6b2b395fe782a183ef9d887f25073ee42afa
LinYangxin <104317359@qq.com>
2017-02-12 17:36:37 +0800

    第五章

--- query.py
+++ query.py
@@ -1,8 +1,4 @@
 from app import db,models
 users = models.User.query.all()
 for u in users:
-    db.session.delete(u)
-posts = models.Post.query.all()
-for p in posts:
-    db.session.delete(p)
-db.session.commit()
+   print(u.nickname+":"+u.email)
